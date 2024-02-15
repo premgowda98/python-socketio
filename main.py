ebcfa49dc5bc97d89df1edab730919ef931a924f
Prem Kumar G <premkumarmvks@gmail.com>
2024-02-15 16:57:46 +0530

    callbacks implementation

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ app = socketio.WSGIApp(sio, static_files={
     '/':'./public/'
 })
 
+def task(sid):
+    sio.sleep(5)
+    sio.emit('from_basckground', {'resp': 'Gowda'}, to=sid)
+
 @sio.event
 def connect(sid, environ):
     # sid is the client id which is connected
     # environ is header containing info
     print('New Client', sid)
+
 
 
 @sio.event
@@ -20,10 +25,11 @@ def disconnect(sid):
 @sio.event
 def from_client(sid, data):
     print(sid, data)
+    sio.start_background_task(task, sid)
     # sio.emit('from_server', {'message': 'from server'}, to=sid)
     # print('emitted')
 
-    return 'from server'
+    return 'from server, you will get results in 5s'
 
 
 if __name__ == '__main__':
--- public/main.js
+++ public/main.js
@@ -1,20 +1,24 @@
 const sio = io();
+const emit_buttton = document.getElementById('emit_buttton')
 
 sio.on('connect', ()=>{
     console.log('Connected')
     // rmit takes two args, event name and event data
+})
+
+emit_buttton.addEventListener('click', ()=>{
     sio.emit('from_client', {
         connected: 'from cient guru'
     }, (response)=>{
         console.log(response)
     })
-
 })
 
 sio.on('disconnect', ()=>{
     console.log('disconnected')
 })
 
-// sio.on('from_server', (data)=>{
-//     console.log(data)
-// })
+sio.on('from_basckground', (data)=>{
+    console.log('This is from background task')
+    console.log(data)
+})
