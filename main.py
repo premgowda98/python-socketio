5d806503f43c8bc35072e961368c9b7eee48ad24
Prem Kumar G <premkumarmvks@gmail.com>
2024-12-06 11:49:43 +0530

    fastapi-websocket

--- main.py
+++ main.py
@@ -1,80 +1,81 @@
-import socketio
-import eventlet
-import random
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
-sio = socketio.Server()
-app = socketio.WSGIApp(sio, static_files={
-    '/':'./public/'
-})
+app = FastAPI()
 
-total_client = 0
-a_groub = 0
-b_group = 0
-
-def cb(data):
-    print(data)
-
-
-def task(sid):
-    sio.sleep(5)
-    # sio.emit('from_basckground', {'resp': 'Gowda'}, to=sid, callback=cb)
-    result = sio.call('from_basckground', {'resp': 'Gowda'}, to=sid)
-    print(result)
-
-@sio.event
-def connect(sid, environ):
-    username = environ.get('HTTP_X_USERNAME')
-    if not username:
-        return False
-    # sid is the client id which is connected
-    # environ is header containing info
-
-    global total_client
-    global a_groub
-    global b_group
-    total_client  = total_client + 1
-    sio.emit('client_count', total_client) # will broadcast for everyone
-
-    if random.random()>0.5:
-        sio.enter_room(sid, 'a')
-        a_groub += 1
-        sio.emit('group_info', f'{username} you are in group A along with {a_groub} members', to='a')
-    else:
-        sio.enter_room(sid, 'b')
-        b_group += 1
-        sio.emit('group_info', f'{username}  you are in group B along with {b_group} members', to='b')
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
 
-@sio.event
-def from_client(sid, data):
-    print(sid, data)
-    sio.start_background_task(task, sid)
-    # sio.emit('from_server', {'message': 'from server'}, to=sid)
-    # print('emitted')
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
 
-    return 'from server, you will get results in 5s'
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
 
-@sio.event
-def disconnect(sid):
-    global total_client
-    global a_groub
-    global b_group
-    total_client  = total_client-1
-    sio.emit('client_count', total_client)
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
 
-    if 'a' in sio.rooms(sid):
-        sio.enter_room(sid, 'a')
-        a_groub -= 1
-        sio.emit('group_info', f'You are in group A along with {a_groub} members', to='a')
-    elif 'b' in sio.rooms(sid):
-        sio.enter_room(sid, 'a')
-        b_group -= 1
-        sio.emit('group_info', f'You are in group B along with {b_group} members', to='b')
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
 
 
-if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+manager = ConnectionManager()
 
-    # command line code
-    # gunicorn -k eventlet -w 1 --reload main:app -b 0.0.0.0:5000
 
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
---
+++ socket-io/main.py
@@ -0,0 +1,80 @@
+import socketio
+import eventlet
+import random
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio, static_files={
+    '/':'./public/'
+})
+
+total_client = 0
+a_groub = 0
+b_group = 0
+
+def cb(data):
+    print(data)
+
+
+def task(sid):
+    sio.sleep(5)
+    # sio.emit('from_basckground', {'resp': 'Gowda'}, to=sid, callback=cb)
+    result = sio.call('from_basckground', {'resp': 'Gowda'}, to=sid)
+    print(result)
+
+@sio.event
+def connect(sid, environ):
+    username = environ.get('HTTP_X_USERNAME')
+    if not username:
+        return False
+    # sid is the client id which is connected
+    # environ is header containing info
+
+    global total_client
+    global a_groub
+    global b_group
+    total_client  = total_client + 1
+    sio.emit('client_count', total_client) # will broadcast for everyone
+
+    if random.random()>0.5:
+        sio.enter_room(sid, 'a')
+        a_groub += 1
+        sio.emit('group_info', f'{username} you are in group A along with {a_groub} members', to='a')
+    else:
+        sio.enter_room(sid, 'b')
+        b_group += 1
+        sio.emit('group_info', f'{username}  you are in group B along with {b_group} members', to='b')
+
+
+@sio.event
+def from_client(sid, data):
+    print(sid, data)
+    sio.start_background_task(task, sid)
+    # sio.emit('from_server', {'message': 'from server'}, to=sid)
+    # print('emitted')
+
+    return 'from server, you will get results in 5s'
+
+@sio.event
+def disconnect(sid):
+    global total_client
+    global a_groub
+    global b_group
+    total_client  = total_client-1
+    sio.emit('client_count', total_client)
+
+    if 'a' in sio.rooms(sid):
+        sio.enter_room(sid, 'a')
+        a_groub -= 1
+        sio.emit('group_info', f'You are in group A along with {a_groub} members', to='a')
+    elif 'b' in sio.rooms(sid):
+        sio.enter_room(sid, 'a')
+        b_group -= 1
+        sio.emit('group_info', f'You are in group B along with {b_group} members', to='b')
+
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+
+    # command line code
+    # gunicorn -k eventlet -w 1 --reload main:app -b 0.0.0.0:5000
+
